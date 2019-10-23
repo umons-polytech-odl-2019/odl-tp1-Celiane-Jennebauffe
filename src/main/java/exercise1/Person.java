a581456f4725e96e986c90b7e8fa3c64be9ef8eb
Céliane <180929@student.umons.ac.be>
2019-10-23 15:01:00 +0200

    Fini ex 1

--- src/main/java/exercise1/Person.java
+++ src/main/java/exercise1/Person.java
@@ -1,4 +1,27 @@
 package exercise1;
 
 class Person {
+	private int age;
+	private String nom;
+	public Person(String nom, int age)
+	{
+		this.nom=nom;
+		this.age=age;
+	}
+	public void setName(String nom)
+	{
+		this.nom=nom;
+	}
+	public String getName()
+	{
+		return nom;
+	}
+	public void setAge(int age)
+	{
+		this.age=age;
+	}
+	public int getAge()
+	{
+		return age;
+	}
 }
